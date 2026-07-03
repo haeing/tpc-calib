c336ee9f94950ca7687080396c3ea4ef3b8dbd30
haeing <hain0817@korea.ac.kr>
2026-07-03 12:42:34 +0900

    cobo

---
+++ cobo-phase/residual.cc
@@ -0,0 +1,28 @@
+const int runnumber = 2447;
+
+void residual()
+{
+  gROOT->SetBatch(kTRUE);
+  TFile *file = TFile::Open(Form("~/data/JPARC2025Nov_root/cobo-phase/run0%d_DstTPCHelixTracking_y0.root",runnumber),"read");
+  string outpdf = Form("result/residual-run0%d.pdf", runnumber);
+  TCanvas *c1 = new TCanvas("c1","c1");
+  
+  gStyle->SetOptStat(0);
+  TPaveText *p = new TPaveText(0.1,0.1,0.9,0.9,"NDC");
+  p->AddText("residual.cc");
+  p->AddText(Form("run0%d",runnumber));
+  TDatime now;
+  p->AddText(Form("Generated at: %04d-%02d-%02d %02d:%02d:%02d",now.GetYear(),now.GetMonth(),now.GetDay(),now.GetHour(),now.GetMinute(),now.GetSecond()));
+  p->Draw();
+  c1->Print((outpdf + "(").c_str());
+  c1->Clear();
+
+  TString hname = "TPCTrk_ResY_vs_Layer_Trk";
+  TH2D *h2 = (TH2D*)file->Get(hname);
+  h2->GetXaxis()->SetTitle("Layer ID");
+  h2->GetYaxis()->SetTitle("Residual Y");
+
+  h2->Draw("colz");
+  c1->Print((outpdf + ")").c_str());
+  
+}
